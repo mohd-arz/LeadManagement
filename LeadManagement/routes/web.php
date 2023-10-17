dfdd7f91b152bf91913fd7d5f044bfd99d15835f
mohd-arz <mohammedarsh75@gmail.com>
2023-10-17 20:03:01 +0530

    added auth middleware also

--- LeadManagement/routes/web.php
+++ LeadManagement/routes/web.php
@@ -43,11 +43,11 @@ Route::get('home',[AuthController::class,'AuthFn'])->middleware('auth')->name('h
 //Executive routes--
 
 //Adding Leads by Executive
-Route::get('/add_lead_page',[CrudController::class,'addLeadPage'])->name('addLeadPage');
+Route::get('/add_lead_page',[CrudController::class,'addLeadPage'])->middleware('auth')->name('addLeadPage');
 Route::post('add_lead',[CrudController::class,'addLead'])->name('addLead');
 
 //Editing Leads by Executive
-Route::get('/edit_lead/{id}',[CrudController::class,'editLeadPage'])->name('editLeadPage');
+Route::get('/edit_lead/{id}',[CrudController::class,'editLeadPage'])->middleware('auth')->name('editLeadPage');
 Route::post('/editing_lead/{id}',[CrudController::class,'editLead'])->name('editLead');
 
 //Deleting Leads for both Admin and Executive
@@ -55,7 +55,7 @@ Route::get('/delete_lead/{id}',[CrudController::class,'deleteLead'])->name('dele
 
 
 //Duplication Handling routes
-Route::get('add_duplicate',[CrudController::class,'addDuplicate'])->name('addDuplicate');
+Route::get('add_duplicate',[CrudController::class,'addDuplicate'])->middleware('auth')->name('addDuplicate');
 Route::get('reject_duplicate',[CrudController::class,'rejectDuplicate'])->name('rejectDuplicate');
 
 //Admin routes--
@@ -64,25 +64,20 @@ Route::get('reject_duplicate',[CrudController::class,'rejectDuplicate'])->name('
 Route::post('/set_status',[CrudController::class,'setStatus'])->name('setStatus');
 
 
-
-
 //Setting Contact Type
 Route::post('/set_option',[CrudController::class,'setOption'])->name('setOption');
 
 
 //filter
-
 Route::post('/filter_category',[CrudController::class,'filterCategory'])->name('filterCategory');
-
 Route::post('/filter_executive',[CrudController::class,'filterExecutive'])->name('filterExecutive');
-
 Route::post('/filter_higher',[CrudController::class,'filterByHigher'])->name('filterByHigher');
 Route::post('/filter_lower',[CrudController::class,'filterByLower'])->name('filterByLower');
 
     
 
 //Admin Middleware
-Route::middleware('admin')->group(function () {
+Route::middleware(['admin','auth'])->group(function () {
     Route::get('leads',[CrudController::class,'leadPage'])->name('leadPage');
     Route::get('/add_lead_admin',[CrudController::class,'addLeadPageAdmin'])->middleware(['admin'])->name('addLeadPageAdmin');
     Route::post('/adding_lead_admin',[CrudController::class,'addLeadAdmin'])->name('addLeadAdmin');
